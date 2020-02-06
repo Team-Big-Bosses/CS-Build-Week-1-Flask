9ea6b27364ce86a5641d76aea93c45972f1537fa
SamH3pn3r <samuelhepner03@gmail.com>
2020-02-05 16:23:03 -0800

    Trying to build the world nbd

--- world.py
+++ world.py
@@ -71,36 +71,63 @@ class World:
             return user
         return None
 
+    def create_world2(self):
+        self.grid = [None] * 10
+        self.width = 10
+        self.height = 10
+        for i in range(len(self.grid)):
+            self.grid[i] = [None] * self.width
+
+        x = -1 # (this will become 0 on the first step)
+        y = 0
+        room_count = 0
+
+        exits = random.randint(0,14)  
+        direction = 1
+
+        previous_room = None
+
+        while (room_count < 100):
+            if (exits < 4):
+                # one exit rooms
+                if (exits == 0):
+                    pass
+                elif (exits == 1):
+                    pass
+                elif (exits == 2):
+                    pass
+                else:
+                    pass
+            elif (exits >= 4 and exits < 10):
+                # two exit rooms
+                if (exits == 4):
+                    pass
+                elif (exits == 5):
+                    pass
+                elif (exits == 6):
+                    pass
+                elif (exits == 7):
+                    pass
+                elif (exits == 8):
+                    pass
+                else:
+                    pass
+            elif (exits >= 10 and exits < 14):
+                # three exit rooms
+                if (exits == 10):
+                    pass
+                elif (exits == 11):
+                    pass
+                elif (exits == 12):
+                    pass
+                else:
+                    pass
+            else:
+                # four exit room
+                pass 
+
+
     def create_world(self):
-        # UPDATE THIS:
-        # Should create 100 procuedurally generated rooms
-        # self.rooms = {
-        #     'outside':  Room("Outside Cave Entrance",
-        #                      "North of you, the cave mount beckons", 1, 1, 1, items=[Item('Torch', 'Lights the way', price=1)]),
-
-        #     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-        # passages run north and east.""", 2, 1, 2, items=[Item('Torch', 'Lights the way', price=1)], store=Store()),
-
-        #     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-        # into the darkness. Ahead to the north, a light flickers in
-        # the distance, but there is no way across the chasm.""", 3, 1, 3),
-
-        #     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-        # to north. The smell of gold permeates the air.""", 4, 2, 2),
-
-        #     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-        # chamber! Sadly, it has already been completely emptied by
-        # earlier adventurers. The only exit is to the south.""", 5, 2, 3),
-        # }
-
-        # # Link rooms together
-        # self.rooms['outside'].connect_rooms('n', self.rooms['foyer'])
-        # self.rooms['foyer'].connect_rooms('n', self.rooms['overlook'])
-        # self.rooms['foyer'].connect_rooms('e', self.rooms['narrow'])
-        # self.rooms['narrow'].connect_rooms('n', self.rooms['treasure'])
-        
-        # self.starting_room = self.rooms['outside']
-        
         # Code from Brett that has been partially modified
         # Initializing the grid
         self.grid = [None] * 10
